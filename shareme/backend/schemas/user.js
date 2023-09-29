6fddfcd3c706272108fe884ab25471018a583373
nithesh2303 <nitheshkanna23@gmail.com>
2023-09-29 20:07:23 +0530

    Updated Schemas

---
+++ shareme/backend/schemas/user.js
@@ -0,0 +1,18 @@
+// default export -> every module - atmost one default export
+export default {
+    name: 'user',
+    title: 'user',
+    type: 'document',
+    fields: [
+        {
+            name: 'UserName',
+            title: 'UserName',
+            type: 'string'
+        },
+        {
+            name: 'Image',
+            title: 'Image',
+            type: 'string'
+        }
+    ]
+}
